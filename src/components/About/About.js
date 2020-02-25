f42a8414e5b39d5748bdf6d160a6efbcef5fec27
Dewinurdin <dewinurdin@gmail.com>
2020-02-25 09:21:35 -0600

    fix paragraph

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,8 +8,9 @@ const About = () => (
   <Container className="banner_text section" id="about" fluid>
     <h1>Deliciousness jumping into the mouth</h1>
     <p> ZiZi Ramen & Sushi is a family owned business that prides itself on serving quality food. </p>
-    <p>Our chefs have worked in the sushi and ramen business for over 10 years. </p>
-    <p>After years of experience in Japanese cuisine, ZiZi Ramen & Sushi hopes to bring a taste of Japan in your area. </p>
+    <p>Our chefs have worked in the sushi and ramen business for over 10 years. 
+        After years of experience in Japanese cuisine, ZiZi Ramen & Sushi hopes to bring a taste of Japan in your area.
+    </p>
     <p>Join us in experiencing a new taste that will leave you wanting more! </p>
     <img src={ramen_img} className="ramen-img" alt="Ramen"/>
   </Container>
